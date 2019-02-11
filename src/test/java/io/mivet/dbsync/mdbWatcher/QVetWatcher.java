684998f221da4c64eafcca4f861a3e54a855f353
Marcos Tenrero <tenrero@aol.com>
2019-02-11 19:58:07 +0100

    After Test teardown. Close file descriptor

--- src/test/java/io/mivet/dbsync/mdbWatcher/QVetWatcher.java
+++ src/test/java/io/mivet/dbsync/mdbWatcher/QVetWatcher.java
@@ -1,18 +1,37 @@
 package io.mivet.dbsync.mdbWatcher;
 
+import org.junit.After;
 import org.junit.Test;
 
 import static org.junit.Assert.assertNotNull;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 
 public class QVetWatcher {
 
+    MdbQVetWatcher mdbQVetWatcher;
+
+    private MdbQVetWatcher openMdbWatcher() {
+        if (mdbQVetWatcher == null) {
+            File db = new File("src/test/resources/q.mdb");
+            mdbQVetWatcher= new MdbQVetWatcher(db.getAbsolutePath());
+        }
+        return mdbQVetWatcher;
+    }
+
+    @After
+    public void closeDescriptor() {
+        try {
+            mdbQVetWatcher.mdbDatabase.close();
+        } catch (IOException e) {
+        }
+    }
+
     @Test
     public void testOpenFile() {
-        File db = new File("src/test/resources/q.mdb");
-        MdbQVetWatcher qWatcher = new MdbQVetWatcher(db.getAbsolutePath());
-        assertNotNull(qWatcher.mdbDatabase);
+        MDBWatcherImpl qWatcher = openMdbWatcher();
+        assertNotNull(qWatcher.mdbDatabase);   
     }
 }
